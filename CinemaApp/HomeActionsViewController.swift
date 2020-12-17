6f5f5e66e883e392b3ba02c3ed02ad621b6d7721
Katya <romanchuk.katerina@gmail.com>
2020-12-17 10:01:02 +0200

    Update HomeActionsViewController.swift

--- CinemaApp/HomeActionsViewController.swift
+++ CinemaApp/HomeActionsViewController.swift
@@ -7,7 +7,6 @@ class HomeActionsViewController: UIViewController {
 
     //@IBOutlet weak var dfdfgd: UIButton!
     fileprivate var eventAPI: EventAPI!
-    //fileprivate var localReplicator: LocalReplicator!
     fileprivate var remoteReplicator: RemoteReplicator!
 
     override func viewDidLoad() {
@@ -16,7 +15,6 @@ class HomeActionsViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         self.eventAPI = EventAPI.sharedInstance
-        //self.localReplicator = LocalReplicator.sharedInstance
         self.remoteReplicator = RemoteReplicator.sharedInstance
     }
 
@@ -24,21 +22,11 @@ class HomeActionsViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-//    @IBAction func debug_only_anonimizeListButtonTapped(_ sender: AnyObject) {
-//        eventAPI.anonimizeAttendeesList()
-//        self.navigationController?.popToRootViewController(animated: true)
-//    }
-
     @IBAction func deleteAllEventsButtonTapped(_ sender: AnyObject) {
         eventAPI.deleteAllEvents()
         self.navigationController?.popToRootViewController(animated: true)
     }
 
-//    @IBAction func restoreEventsButtonTapped(_ sender: AnyObject) {
-//        //localReplicator.fetchData()
-//        NotificationCenter.default.post(name: Notification.Name(rawValue: "setStateLoading"), object: nil)
-//        self.navigationController?.popToRootViewController(animated: true)
-//    }
     @IBAction func replicateRemoteDataButtonTapped(_ sender: AnyObject) {
         remoteReplicator.fetchData()
         NotificationCenter.default.post(name: Notification.Name(rawValue: "setStateLoading"), object: nil)
